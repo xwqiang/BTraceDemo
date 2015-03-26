1abbf45f3c5b125d6fcd606870974b37e2347350
xwqiang <642143714@qq.com>
2015-03-26 13:27:54 +0800

    'init'

---
+++ src/my/app/test/HelloWorld.java
@@ -0,0 +1,26 @@
+package my.app.test;
+
+import java.util.Random;
+
+public class HelloWorld {
+	public static void main(String[] args) throws Exception {
+		//CaseObject object = new CaseObject();
+		while (true) {
+			Random random = new Random();
+			execute(random.nextInt(4000));
+			
+			//object.execute(random.nextInt(4000));
+		}
+		
+		
+		
+	}
+	public static Integer execute(int sleepTime) {
+		try {
+			Thread.sleep(sleepTime);
+		} catch (Exception e) {
+		}
+		System.out.println("sleep time is=>"+sleepTime);
+		return 0;
+	}
+}
